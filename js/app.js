2c437f0dcb72b8c1c1f4d625822404dd2664f754
Ali Habibie <malizzulhaq08@gmail.com>
2022-02-20 09:25:45 +0700

    new

--- js/app.js
+++ js/app.js
@@ -1,3 +1,34 @@
+Vue.component('greeting', {
+    template: `
+        <div>
+            <h1>Hello Vue from Component!</h1>
+        </div>
+    `
+});
+
+Vue.component('button-counter', {
+    data: function(){
+        return {
+            count : 0
+        }
+    },
+    template: `
+        <button v-on:click="count++">Clicked {{ count }} times</button>
+    `
+});
+
+//data prop di dpt dari luar dgn directive binding 'post'. dan v-html utk generate konten html
+Vue.component('blog-post', {
+    props: ['post'],
+    template:
+    `
+    <div>
+        <h3>{{post.title}}</h3>
+        <div v-html="post.content"></div>
+    </div>
+    `
+});
+
 var app = new Vue({
     el: '#test',
     data: {
@@ -14,7 +45,12 @@ var app = new Vue({
         int2: 15,
         result: null,
         kilometers: 0,
-        meters: 0
+        meters: 0,
+        posts: [
+            { id: 1, title: 'Marvel VS DC', content: 'The Legendary battle between two big fandom heroes of multiversal studios' },
+            { id: 2, title: 'Spiderman identity revealed?', content: 'Identity of spiderman is revealed by the mighty hero of eart 616 Mysterio!?' },
+            { id: 3, title: 'Fantastic 4 Go International', content: 'Reed Richard AKA Mr. Fantastic will open the biggest even in century to research the multitalent of human.' },
+        ]
     },
     //computed saved in var, same as cache
     computed: {
